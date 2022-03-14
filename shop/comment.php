340d89899cd1e419c3b245e98d15b71809ebae2e
Denis <45605089+d3nsi@users.noreply.github.com>
2022-03-14 19:19:54 +0700

    Add files via upload

--- shop/comment.php
+++ shop/comment.php
@@ -122,6 +122,7 @@
           </div>
         </form>
       </div>
+    </div>
     <?php
       } else {
         $name = trim($name);
@@ -129,16 +130,99 @@
         $date = trim($date);
         $topic = trim($topic);
 
-        setcookie('name', $name);
-        setcookie('email', $email);
-        setcookie('date', $date);
-        setcookie('gender', $gender);
+        if (preg_match('/[^\sa-zA-Z0-9]/', $name, $res)) {
+            echo "<br>";
+            echo "<h2>В имени недопустимый символ: $res[0]</h2>";
+            $nameChar = $res[0];
+        }
 
-        $stmt = $link->prepare("INSERT INTO feedback (feedback_name, feedback_mail, feedback_birthday, feedback_gender, feedback_topic, feedback_msg) VALUES (?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("ssssss", $name, $email, $date, $gender, $topic, $msg);
-        $stmt->execute();
-        echo "<h1>Форма отправлена</h1>";
-      }
+        if (preg_match('/[^a-zA-Z0-9]/', $topic, $res)) {
+            echo "<br>";
+            echo "<h2>В название темы недопустимый символ: $res[0]</h2>";
+            $topicChar = $res[0];
+        }
+
+        if ($nameChar or $topicChar) { ?>
+          <div class="layout">
+            <div class="form">
+              <form action="#" method="POST">
+                <?php
+                  if ($nameChar) {
+                    $style = 'border-bottom: 1px solid red';
+                  } else {
+                    $style = '';
+                  }
+                  $value = $name;
+                  echo "<div class='form__input' style='$style'>";
+                    echo "<label class='form__label'>Имя:</label>";
+                    #echo "<label class='form__labe' style='color: red; font-size: 12px'>Введите имя</label>";
+                    echo "<input class='form__input-text' type='input' name='name' value='$value'>";
+                  echo "</div>";
+                  
+                  $style = '';
+                  echo "<div class='form__input' style='$style'>";
+                    echo "<label class='form__label'>e-mail:</label>";
+                    echo "<input class='form__input-text' type='email' name='email' value='$email'>";
+                  echo "</div>";
+  
+                  echo "<div class='form__input' style='$style'>";
+                    echo "<label class='form__label'>Год рождения:</label>";
+                    echo "<input class='form__input-text' type='date' name='date' value='$date'>";
+                  echo "</div>";
+
+                  echo "<div class='form__input' style='$style'>";
+                  echo "<label class='form__label'>Пол:</label>";
+                  echo "<div class='form__radio'>";
+                  echo "<input type='radio' name='gender' value='man'$styleMan>";
+                  echo "<label class='form__label'>М</label>";
+                  echo "<input type='radio' name='gender' value='woman'$styleWoman>";
+                  echo "<label class='form__label'>Ж</label>";
+  
+                  echo "</div>";
+                  echo "</div>";
+  
+                  if ($topicChar) {
+                    $style = 'border-bottom: 1px solid red';
+                  } else {
+                    $style = '';
+                  }
+                  
+                  $value = $topic;
+                  echo "<div class='form__input' style='$style'>";
+                  echo "<label class='form__label'>Тема обращения:</label>";
+                  echo "<input class='form__input-text' type='input' name='topic' value='$value'>";
+                  echo "</div>";
+                  $style = "";
+                  echo "<div class='form__input' style='$style'>";
+                  echo "<label class='form__label'>Суть вопроса:</label>";
+                  echo "<textarea class='form__textarea' name='msg'>$msg</textarea>";
+                  echo "</div>";
+                  echo "<div class='form__input' style='$style'>";
+  ?>
+                  <div class='form__checkbox'>
+                      <input type='checkbox' value='check' name='checkbox'>
+                      <label class='form__label'>С контактом ознакомлен</label>
+                  </div>
+                  </div>
+
+                  <div class="form__button-box">
+                      <input class="form__button" type="submit" value="Отправить">
+                  </div>
+                  </form>
+            </div>
+        <?php } else {
+                setcookie('name', $name);
+                setcookie('email', $email);
+                setcookie('date', $date);
+                setcookie('gender', $gender);
+
+                $stmt = $link->prepare("INSERT INTO feedback (feedback_name, feedback_mail, feedback_birthday, feedback_gender, feedback_topic, feedback_msg) VALUES (?, ?, ?, ?, ?, ?)");
+                $stmt->bind_param("ssssss", $name, $email, $date, $gender, $topic, $msg);
+                $stmt->execute();
+                echo "<h1>Форма отправлена</h1>";
+              }
+            }
+           
     ?> 
   </div>
 </body>
